de1e014ae0ba0bd3d49b6c017aeec61630dafe76
Kristian <krand17@student.sdu.dk>
2021-10-18 14:18:42 +0200

    Tilføjelse af filer til app

---
+++ Eksamens_App/AppView.swift
@@ -0,0 +1,20 @@
+//
+//  AppView.swift
+//  
+//
+//  Created by Kristian Andersen on 18/10/2021.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
