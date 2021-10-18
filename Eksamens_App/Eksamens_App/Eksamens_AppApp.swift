ee0bad1b3f6ff2bac713c208c22f8e4ec6b3162d
Kristian <krand17@student.sdu.dk>
2021-10-18 14:12:28 +0200

    Tilføjelse af app til GitHub

---
+++ Eksamens_App/Eksamens_App/Eksamens_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eksamens_AppApp.swift
+//  Eksamens_App
+//
+//  Created by Kristian Andersen on 18/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Eksamens_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
