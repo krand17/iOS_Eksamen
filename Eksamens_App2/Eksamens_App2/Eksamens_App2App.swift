abd9b7cad286437042862bd45db318d42a3be918
Kristian <krand17@student.sdu.dk>
2021-10-26 17:35:41 +0200

    App2

---
+++ Eksamens_App2/Eksamens_App2/Eksamens_App2App.swift
@@ -0,0 +1,17 @@
+//
+//  Eksamens_App2App.swift
+//  Eksamens_App2
+//
+//  Created by Kristian Andersen on 26/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Eksamens_App2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
