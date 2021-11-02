83b617f0895a8f2bb163a04d54c20fbe9cf9d7e6
Kristian <krand17@student.sdu.dk>
2021-11-02 13:19:41 +0100

    MainPageView added

---
+++ Eksamens_App2/Eksamens_App2/MainPageView.swift
@@ -0,0 +1,20 @@
+//
+//  MainPageView.swift
+//  Eksamens_App2
+//
+//  Created by Kristian Andersen on 02/11/2021.
+//
+
+import SwiftUI
+
+struct MainPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainPageView()
+    }
+}
